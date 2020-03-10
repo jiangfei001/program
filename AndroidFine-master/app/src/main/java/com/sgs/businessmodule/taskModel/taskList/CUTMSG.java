bf4409fe4ebcb52ef86de7a9f046cca7eaa3e3f8
01371245 <FEIJIANG@SFMAIL.SF-EXPRESS.COM>
2020-03-10 22:10:28 +0800

    修改优先级

--- AndroidFine-master/app/src/main/java/com/sgs/businessmodule/taskModel/taskList/CUTMSG.java
+++ AndroidFine-master/app/src/main/java/com/sgs/businessmodule/taskModel/taskList/CUTMSG.java
@@ -6,15 +6,19 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import com.sgs.businessmodule.taskModel.TVTask;
 import com.sgs.businessmodule.taskUtil.cutMsg.MsgDbManager;
+import com.sgs.businessmodule.taskUtil.cutMsg.MuTerminalBack;
 import com.sgs.businessmodule.taskUtil.cutMsg.MuTerminalMsg;
 import com.sgs.middle.eventControlModel.Event;
 import com.sgs.middle.eventControlModel.EventEnum;
 
 import org.greenrobot.eventbus.EventBus;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CUTMSG extends TVTask {
+
+    ArrayList<MuTerminalBack> muTerminalMsgs = new ArrayList<>();
 
     @Override
     public void runTv() {
@@ -24,6 +28,12 @@ public class CUTMSG extends TVTask {
         MuTerminalMsg muTerminalMsg = JSON.parseObject(prog, new TypeReference<MuTerminalMsg>() {
         });
 
+        MuTerminalBack muTerminalBack = new MuTerminalBack();
+        muTerminalBack.setMsgStatus("3");
+        muTerminalBack.setMsgId(muTerminalMsg.getId());
+        muTerminalBack.setFinishTime(muTerminalMsg.getHasplay());
+        muTerminalMsgs.add(muTerminalBack);
+
         Event event = new Event();
         HashMap<EventEnum, Object> params = new HashMap();
         params.put(EventEnum.EVENT_TEST_MSG1_KEY_CUTMSG, muTerminalMsg);
@@ -31,5 +41,10 @@ public class CUTMSG extends TVTask {
         event.setId(EventEnum.EVENT_TEST_SETCUTMSG);
         EventBus.getDefault().post(event);
         System.out.println("muTerminalMsg.toString()" + muTerminalMsg.toString());
+    }
+
+    @Override
+    public void setResult() {
+        responseEntity.setResult(com.alibaba.fastjson.JSON.toJSONString(muTerminalMsgs));
     }
 }
